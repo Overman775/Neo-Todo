84b325058e508cfa41e9993879ad5aaae53db81b
Alexey Z <47769319+Overman775@users.noreply.github.com>
2020-04-22 22:00:54 +0500

    initial commit

---
+++ lib/pages/todo.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class TodoPage extends StatefulWidget {
+  TodoPage({Key key}) : super(key: key);
+
+  @override
+  _TodoPageState createState() => _TodoPageState();
+}
+
+class _TodoPageState extends State<TodoPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+       child: null,
+    );
+  }
+}
