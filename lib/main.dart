294baaa90873b2c8bf0e23daf8ac626fb9a3922b
Alexey Z <47769319+Overman775@users.noreply.github.com>
2020-05-05 22:57:04 +0500

    add meumorphic package

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_neumorphic/flutter_neumorphic.dart';
 import 'package:provider/provider.dart';
 import 'package:todolist/pages/add_task.dart';
+import 'package:todolist/pages/main.dart';
 import 'package:todolist/pages/todo.dart';
 import 'package:todolist/models/todo.dart';
 import 'package:todolist/style.dart';
@@ -13,32 +15,54 @@ class MyApp extends StatelessWidget {
     return MultiProvider(
       providers: [ChangeNotifierProvider(create: (_) => TodoModel())],
       child: MaterialApp(
+        debugShowCheckedModeBanner: false,
         title: 'Todo List',
         theme: ThemeData(
-          primarySwatch: Style.primaryColorMaterial,
-          primaryColor: Style.primaryColor,
-          appBarTheme: AppBarTheme(
-            elevation: 0.0,
-            color: Style.bgColor,
-            textTheme: TextTheme(
-              title: Style.headerTextStyle
-            ),
-            iconTheme: IconThemeData(
-              color: Style.primaryColor
-            )
-          ),
-          scaffoldBackgroundColor: Style.bgColor
-        ),
+            primarySwatch: Style.primaryColorMaterial,
+            primaryColor: Style.primaryColor,
+            appBarTheme: AppBarTheme(
+                elevation: 0.0,
+                color: Style.bgColor,
+                textTheme: TextTheme(title: Style.headerTextStyle),
+                iconTheme: IconThemeData(color: Style.primaryColor)),
+            scaffoldBackgroundColor: Style.bgColor),
         initialRoute: '/',
         onGenerateRoute: (RouteSettings settings) {
-          var routes = <String, WidgetBuilder>{
-            "/": (context) => TodoPage(),
-            "/task": (context) => AddTask(settings.arguments),
+          var routes = <String, Widget>{
+            "/": MainPage(),
+            "/list": TodoPage(),
+            "/task": AddTask(settings.arguments),
           };
-          WidgetBuilder builder = routes[settings.name];
+          WidgetBuilder builder = (context) => NeumorphicWidget(routes[settings.name]);
           return MaterialPageRoute(builder: (context) => builder(context));
         },
       ),
     );
+  }
+}
+
+class NeumorphicWidget extends StatelessWidget {
+  final child;
+
+  const NeumorphicWidget(this.child, {Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return NeumorphicTheme(
+        usedTheme: UsedTheme.LIGHT,
+        theme: NeumorphicThemeData(
+          baseColor: Style.bgColor,
+          intensity: 0.5,
+          lightSource: LightSource.topLeft,
+          depth: 10,
+        ),
+        darkTheme: NeumorphicThemeData(
+          //TODO Add black theme
+          baseColor: Color(0xFF3E3E3E),
+          intensity: 0.5,
+          lightSource: LightSource.topLeft,
+          depth: 6,
+        ),
+        child: child);
   }
 }
