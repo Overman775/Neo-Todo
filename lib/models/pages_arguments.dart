23471d211a631c47e262329138c90929b5c7f5cb
Alexey Z <47769319+Overman775@users.noreply.github.com>
2020-05-28 04:43:12 +0500

    factory CardPosition

--- lib/models/pages_arguments.dart
+++ lib/models/pages_arguments.dart
@@ -2,18 +2,18 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/widgets.dart';
 import 'package:todolist/models/todo_models.dart';
 
-class ItemPageArguments{
+class ItemPageArguments {
   final TodoItem item;
   final TodoCategory category;
 
   ItemPageArguments({this.item, @required this.category});
 }
 
-class MainPageArguments{
+class MainPageArguments {
   final TodoCategory category;
   final CardPosition cardPosition;
 
-  MainPageArguments({@required this.category, @required this.cardPosition});
+  MainPageArguments({this.category, @required this.cardPosition});
 }
 
 class CardPosition {
@@ -21,6 +21,24 @@ class CardPosition {
   final double top;
   final double right;
   final double bottom;
+
+  factory CardPosition.getPosition(BuildContext context) {
+    //search widget
+    final RenderBox renderBox = context.findRenderObject();
+    //get position widget
+    final position = renderBox.localToGlobal(Offset.zero);
+    //get size widget
+    final size = renderBox.size;
+    //get screen size
+    var screenWidth = MediaQuery.of(context).size.width;
+    var screenHeight = MediaQuery.of(context).size.height;
+
+    return CardPosition(
+        left: position.dx,
+        top: position.dy,
+        right: screenWidth - size.width - position.dx,
+        bottom: screenHeight - size.height - position.dy);
+  }
 
   CardPosition({this.left = 0, this.top = 0, this.right = 0, this.bottom = 0});
 }
